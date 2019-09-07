765a36ff110f70f72f19e72a7cae23b2b704034b
Oscar Shoudt <shoudto@gmail.com>
2019-09-07 03:37:39 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,5 +1,6 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
@@ -40,5 +41,27 @@ class InteractiveRecord
       values << "'#{send(column_name)}'" unless send(column_name).nil?
     end
     values.join(", ")
+  end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert}(#{col_names_for_insert}) VALUES (#{values_for_insert})"
+
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE name = ?"
+
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attribute)
+    column_name = attribute.keys[0].to_s
+    value_of_column = attribute.values[0]
+
+    sql = "SELECT * FROM #{table_name} WHERE #{column_name} = ?"
+
+    DB[:conn].execute(sql, value_of_column)
   end
 end

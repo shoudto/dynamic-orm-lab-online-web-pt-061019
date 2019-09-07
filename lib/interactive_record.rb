8f8aff43ca65036b62c8f7af0fd742ed85048507
Oscar Shoudt <shoudto@gmail.com>
2019-09-07 03:17:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
     values = []
 
     self.class.column_names.each do |column_name|
-      values << "#{send(column_name)}" unless send(column_name).nil?
+      values << "'#{send(column_name)}'" unless send(column_name).nil?
     end
     values.join(", ")
   end
